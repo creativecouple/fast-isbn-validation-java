554b4a7cdc7eecaef7fd2192cb18904badc4125b
Peter Liske <peter.liske@gmx.net>
2022-08-21 06:43:55 +0200

    add disclaimer

--- generate/GenerateIsbnRanges.java
+++ generate/GenerateIsbnRanges.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 CreativeCouple
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import org.w3c.dom.*;
 
 import javax.xml.parsers.*;
@@ -10,6 +33,29 @@ import static java.util.stream.Collectors.toList;
 
 public class GenerateIsbnRanges {
 
+    private static String DISCLAIMER = "/*\n" +
+            " * The MIT License (MIT)\n" +
+            " *\n" +
+            " * Copyright (c) 2022 CreativeCouple\n" +
+            " *\n" +
+            " * Permission is hereby granted, free of charge, to any person obtaining a copy of\n" +
+            " * this software and associated documentation files (the \"Software\"), to deal in\n" +
+            " * the Software without restriction, including without limitation the rights to\n" +
+            " * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of\n" +
+            " * the Software, and to permit persons to whom the Software is furnished to do so,\n" +
+            " * subject to the following conditions:\n" +
+            " *\n" +
+            " * The above copyright notice and this permission notice shall be included in all\n" +
+            " * copies or substantial portions of the Software.\n" +
+            " *\n" +
+            " * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
+            " * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS\n" +
+            " * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR\n" +
+            " * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER\n" +
+            " * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN\n" +
+            " * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n" +
+            " */";
+
     public static void main(String[] args) throws Exception {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = dbf.newDocumentBuilder();
@@ -19,6 +65,7 @@ public class GenerateIsbnRanges {
         final String messageSerialNumber = first(doc, "MessageSerialNumber").getTextContent();
         final String messageDate = first(doc, "MessageDate").getTextContent();
 
+        out.println(DISCLAIMER);
         out.println("package de.creativecouple.validation.isbn;");
         out.println("import java.util.HashMap;");
         out.println("/**");
