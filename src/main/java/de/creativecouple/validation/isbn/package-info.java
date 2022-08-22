e7019fb08ded9904c2984249c4410b14316e1fe4
Peter Liske <peter.liske@gmx.net>
2022-08-22 11:47:38 +0200

    add package info

---
+++ src/main/java/de/creativecouple/validation/isbn/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Parse/format/validate ISBNs with the official ISBN ranges definition.
+ * The parser is optimized for speed (>5k ops/ms).
+ *
+ * @since 0.1.0
+ */
+package de.creativecouple.validation.isbn;
