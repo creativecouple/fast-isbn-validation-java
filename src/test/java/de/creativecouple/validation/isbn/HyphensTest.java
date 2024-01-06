4e81ac35ed8677aeb5106c80f6123cee0d040ede
Peter Liske <peter.liske@gmx.net>
2024-01-06 10:57:32 +0100

    organize imports

--- src/test/java/de/creativecouple/validation/isbn/HyphensTest.java
+++ src/test/java/de/creativecouple/validation/isbn/HyphensTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Named.named;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
